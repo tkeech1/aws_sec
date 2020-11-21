99d22d17f89198211095f46b14bd03b924f04b18
Todd Keech <tkeech1@hotmail.com>
2020-11-21 18:23:30 -0500

    removed mwa

--- modules/mwa/ecr/output.tf
+++
@@ -1,3 +0,0 @@
-output "ecr_repository_url" {
-  value = aws_ecr_repository.mwa_ecr_repo.repository_url
-}
--- modules/mwa/s3web/output.tf
+++
@@ -1,4 +0,0 @@
-output "mwa_website_endpoint" {
-  value = aws_s3_bucket.s3_static_web.website_endpoint
-}
-
